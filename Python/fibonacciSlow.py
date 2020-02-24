2d74a09b76a85459bdcd8052b1a0a52675819e43
Ruan <ruan4f@gmail.com>
2020-02-23 22:02:19 -0300

    Create fibonacciSlow.py

---
+++ Python/fibonacciSlow.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+  if n == 1:
+    return 1
+  elif n == 2:
+    return 1
+  elif n > 2:
+    return fibonacci(n-1) + fibonacci(n-2)
+
+for n in  range(1,101):
+  print(n,":", fibonacci(n)) 
