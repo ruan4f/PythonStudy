7b377e01d991bfb5fc4c0a7fbb377ddcace258ca
Ruan <ruan4f@gmail.com>
2020-02-23 22:00:11 -0300

    Create fibonacciMemoization.py

---
+++ Python/fibonacciMemoization.py
@@ -0,0 +1,23 @@
+
+fibonacci_cache= {}
+
+def fibonacci(n):
+  # If we have cached the value, then return it
+  if n in fibonacci_cache:
+    return fibonacci_cache[n]
+
+  # Compute the Nth term
+  if n == 1:
+    value = 1
+  elif n == 2:
+    value = 1
+  elif n >2:
+    value = fibonacci(n-1) + fibonacci(n-2)
+
+  #Cache the value and return it
+  fibonacci_cache[n] = value
+  return value
+
+
+for n in range(1,101):
+  print(n, ":", fibonacci(n))
